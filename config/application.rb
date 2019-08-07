6f8200892b5e5941b24d3f69f86263af4ece0520
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2019-08-07 20:10:31 +0200

    Use dotenv gem, .env.#{Rails.env} contains database name

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,13 @@ require 'sprockets/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+unless Rails.env.production?
+  Dotenv.load(".env.#{Rails.env}.local",
+              ".env.#{Rails.env}",
+              '.env.local',
+              '.env')
+end
+
 module Releasely
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
