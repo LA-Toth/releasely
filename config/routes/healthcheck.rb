LA-Toth/church-site

Add /healthcheck endpoint with tests, / is redirected to it
The controller and the routing tests (eg. / goes to healthcheck#index)
go into separate _spec files.

config/routes/healthcheck.rb
@@ -1,0 +1,3 @@
+ChurchSite::Application.routes.draw do
+  get :healthcheck, to: 'healthcheck#index'
+end

LA-Toth/releasely

config/routes/healthcheck.rb
@@ -1,0 +1,3 @@
+Releasely::Application.routes.draw do
+  get :healthcheck, to: 'healthcheck#index'
+end