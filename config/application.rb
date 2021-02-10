db6bd72f095d1f64ebf273ff2d2384d4a30f5d46
Laszlo Attila Toth <laszlo.attila.toth@gmail.com>
2021-02-10 17:23:09 +0100

    generators: gen only model, controller, view and routes

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,13 @@ module Releasely
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.test_framework nil
+      g.assets false
+      g.helper false
+      g.stylesheets false
+      g.javascripts false
+    end
   end
 end
